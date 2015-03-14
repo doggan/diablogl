7242050e9e08731fee3e8194c905917d774304ae
Shyam Guthikonda <shyamguth@gmail.com>
2015-03-14 15:38:13 -0400

    Add gulp notify to lint task.

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -4,13 +4,31 @@ var jshint = require('gulp-jshint');
 var config = require('../config');
 var reporter = require('jshint-stylish');
 var map = require('map-stream');
+var path = require('path');
+var events = require('events');
+var emmitter = new events.EventEmitter();
+
+var notify = require("gulp-notify");
 
 var errorReporter = function(file, cb) {
     return map(function(file, cb) {
         if (!file.jshint.success) {
+            file.jshint.results.forEach(function(err) {
+                if (err) {
+                    var msg = [
+                        path.basename(file.path),
+                        'Line: ' + err.error.line,
+                        'Reason: ' + err.error.reason
+                    ];
+
+                    emmitter.emit('error', new Error(msg.join('\n')));
+                }
+            });
+
             gutil.beep();
         }
         cb(null, file);
+
     });
 };
 
@@ -18,5 +36,8 @@ gulp.task('lint', function() {
     return gulp.src(config.lint.src)
         .pipe(jshint())
         .pipe(jshint.reporter(reporter))
-        .pipe(errorReporter());
+        .pipe(errorReporter())
+        .on('error', notify.onError(function(err) {
+            return err.message;
+        }));
 });
