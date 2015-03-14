edae09b4c940784ffcb2666319950e74c7db85b2
Shyam Guthikonda <shyamguth@gmail.com>
2015-03-14 14:49:28 -0400

    Get lint task working with 'beep' event on error.

--- gulp/config.js
+++ gulp/config.js
@@ -1,25 +1,22 @@
-var dest = "./client";
-var src = '.';
-
 module.exports = {
     browserSync: {
         server: {
             // Serve up our build folder
-            baseDir: dest
+            baseDir: './client'
         }
     },
     browserify: {
         // A separate bundle will be generated for each
         // bundle config in the list below
         bundleConfigs: [{
-            entries: src + '/index.js',
-            dest: dest,
+            entries: './lib/index.js',
+            dest: './client',
             outputName: 'index.js',
             // list of externally available modules to exclude from the bundle
             external: []
         }]
     },
     lint: {
-        src: "./*.js"
+        src: "./lib/**/*.js"
     }
 };
--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
 var config = require('../config');
 var reporter = require('jshint-stylish');
+var map = require('map-stream');
+
+var errorReporter = map(function(file, cb) {
+    if (!file.jshint.success) {
+        gutil.beep();
+    }
+    cb(null, file);
+});
 
 gulp.task('lint', function() {
     return gulp.src(config.lint.src)
         .pipe(jshint())
-        .pipe(jshint.reporter(reporter));
+        .pipe(jshint.reporter(reporter))
+        .pipe(errorReporter);
 });
